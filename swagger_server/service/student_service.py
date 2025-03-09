ff4522322d8091326b67348cfb9a3a9d8eb25444
Liorah <monteiroliorah@gmail.com>
2025-03-09 20:17:59 +0100

    updated service

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -10,22 +10,20 @@ db = client["students_db"]
 students_collection = db["students"]
 
 def add(student=None):
-    try:
-        student_exists = students_collection.find_one(
-            {"first_name": student.first_name, "last_name": student.last_name}
-        )
+   
+    student_exists = students_collection.find_one(
+        {"first_name": student.first_name, "last_name": student.last_name}
+    )
 
-        if student_exists :
-            return "already exists", 409
+    if student_exists :
+        return "already exists", 409
 
-        student_data = student.to_dict()
-        insert_result = students_collection.insert_one(student_data)
-        
-        student.student_id = str(insert_result.inserted_id)
-        
-        return student.student_id
-    except Exception as e:
-        return "Internal server error", 500
+    student_data = student.to_dict()
+    insert_result = students_collection.insert_one(student_data)
+    
+    student.student_id = str(insert_result.inserted_id)
+    
+    return student.student_id
 
 def get_by_id(student_id=None):
     student = students_collection.find_one({"_id": ObjectId(student_id)})
