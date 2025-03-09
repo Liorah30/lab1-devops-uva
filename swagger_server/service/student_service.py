ee6d5696ccecfd5cb61b48fbe704df744b0f788f
Liorah <monteiroliorah@gmail.com>
2025-03-09 20:17:59 +0100

    updated service
    updated
    
    removed logs
    
    clean code
    
    cleaned code and final deplooyment

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 from functools import reduce
-
+import uuid
 from pymongo import MongoClient
 from bson import ObjectId
 
@@ -10,30 +10,31 @@ db = client["students_db"]
 students_collection = db["students"]
 
 def add(student=None):
-    try:
-        student_exists = students_collection.find_one(
-            {"first_name": student.first_name, "last_name": student.last_name}
-        )
 
-        if student_exists :
-            return "already exists", 409
+    student_exists = students_collection.find_one(
+        {
+        "first_name": student.first_name, 
+        "last_name": student.last_name }
+    )
 
-        student_data = student.to_dict()
-        insert_result = students_collection.insert_one(student_data)
-        
-        student.student_id = str(insert_result.inserted_id)
-        
-        return student.student_id
-    except Exception as e:
-        return "Internal server error", 500
+    if student_exists :
+        return "already exists", 409
+
+    student_data = student.to_dict()
+    insert_result = students_collection.insert_one(student_data)
+    
+    return str(insert_result.inserted_id)
 
 def get_by_id(student_id=None):
-    student = students_collection.find_one({"_id": ObjectId(student_id)})
-    if not student:
-        return 'not found', 404
-    student['student_id'] = str(student["_id"])
-
-    return student
+    try:
+        student = students_collection.find_one({"_id": ObjectId(student_id)})
+        if not student:
+            return 'not found', 404
+        student['student_id'] = str(student["_id"])
+        del student['_id'] #removing the mongodb's default id because it is not needed and creates error
+        return student
+    except Exception:
+        return 'invalid Id format', 400
 
 
 def delete(student_id=None):
@@ -41,4 +42,4 @@ def delete(student_id=None):
     if result.deleted_count == 0:
         return 'not found', 404
 
-    return str(student_id)
+    return student_id
